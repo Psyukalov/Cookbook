7d31f04e4573d5664774fba351e17906fedb3cfa
Vladimir Psyukalov <psyukalov@gmail.com>
2016-06-10 11:35:00 +0300

    Initial Commit

---
+++ Cookbook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cookbook
+//
+//  Created by Vladimir Psyukalov on 10.06.16.
+//  Copyright © 2016 com.NataliaZubareva.Cookbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cookbook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cookbook
+//
+//  Created by Vladimir Psyukalov on 10.06.16.
+//  Copyright © 2016 com.NataliaZubareva.Cookbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
